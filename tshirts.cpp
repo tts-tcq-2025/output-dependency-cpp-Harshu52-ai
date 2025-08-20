a3944741b0e2d607534a09dc4f128b45129d29d6
Harshita Sinha <204292568+Harshu52-ai@users.noreply.github.com>
2025-08-20 11:14:34 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -14,13 +14,14 @@ char size(int cms) {
     }
     return sizeName;
 }
- 
+
 void testTshirtSize() {
     std::cout << "\nTshirt size test\n";
     assert(size(37) == 'S');
     assert(size(38) == 'L');
     assert(size(40) == 'M');
-    assert(size(42) == 'M');
+    assert(size(42) == 'L');
     std::cout << "All is well (maybe!)\n";
 }
+
 
